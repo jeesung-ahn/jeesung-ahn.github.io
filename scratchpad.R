28965e48d55f2fd5481811da45158c61b31a0d00
jeesung-ahn <jeesung@sas.upenn.edu>
2022-12-13 12:56:34 -0500

    research tbu

---
+++ scratchpad.R
@@ -0,0 +1,84 @@
+
+**BEHAVIORAL AND NEURAL EFFICIENT CODING OF SPEED**  
+  *[\[JNeurosci, 2022\]](https://www.jneurosci.org/content/42/14/2951)  [\[V-VSS 2021 Poster\]](https://www.youtube.com/watch?v=W5DH4h2dH8Y)  [\[GitHub\]](https://github.com/lingqiz/Speed_Prior_2021)* <br>
+  
+  <div class="row">
+    <div class="column">
+      <img class="proj-image" src="/assets/img/speedPrior.png" style="height: 100%; width: 100%; object-fit: contain">
+        </div>
+        
+        <div class="column" markdown="1">
+          We built an efficient encoding, Bayeisan decoding model for human speed perception in a psychophysical experiment. The model makes specific perdictions regarding the neural encoding characteristics of retinal speed, which we validated by analyzing electrophysiology recording of MT neurons.
+        </div>
+          </div>
+          
+          <br>
+          **BAYESIAN IMAGE RECONSTRUCTION FROM CONE MOSAIC SIGNAL**  
+          *[\[eLife, 2022\]](https://elifesciences.org/articles/71132)  [\[V-VSS 2020 Talk\]](https://youtu.be/d5qI0FNCAv4)  [\[GitHub\]](https://github.com/isetbio/ISETImagePipeline)* <br>
+          
+          <div class="row">
+            <div class="column">
+              <img class="proj-image" src="/assets/img/imageRecon.png" style="height: 100%; width: 100%; object-fit: contain">
+                </div>
+                
+                <div class="column" markdown="1">
+                  We built a Bayesian image reconstruction algorithm from cone excitations based on an accurate model of human early vision, in order to understand information loss at the very first step of visual processing. Our model enables quantitative analysis of retinal mosaic design, visualization, and the more "traditional" ideal observer type of analysis.
+                </div>
+                  </div>
+                  
+                  <br>
+                  **VISUAL ORIENTATION ENCODING IN INDIVIDUALS WITH AUTISM**  
+                  *[\[PLOS Biology, 2021\]](https://doi.org/10.1371/journal.pbio.3001215)  [\[Primer\]](https://doi.org/10.1371/journal.pbio.3001293)  [\[Data+Code\]](https://github.com/lingqiz/ASD_Encoding_2020)* <br>
+                  
+                  <div class="row">
+                    <div class="column">
+                      <img class="proj-image" src="/assets/img/encodingASD.png" style="height: 100%; width: 100%; object-fit: contain">
+                        </div>
+                        
+                        <div class="column" markdown="1">
+                          We compared the accuracy of visual orientation encoding between neurotypical and ASD groups using an information theoretic measure. We found that the ASD group starts with an overall lower encoding capacity, which does not improve when presented with performance feedback. They are also less adaptive to the stimulus statistics in contrast to the neurotypical subjects.
+                        </div>
+                          </div>
+                          
+                          <br>
+                          **PSYCHOPHYSICS WITH DEEP NEURAL NETWORKS**  
+                          *[\[CCN, 2019\]](https://ccneuro.org/2019/proceedings/0000585.pdf)* <br>
+                          
+                          <div class="row">
+                            <div class="column">
+                              <img class="proj-image" src="/assets/img/ccn2019.png" style="height: 100%; width: 100%; object-fit: contain">
+                                </div>
+                                
+                                <div class="column" markdown="1">
+                                  We showed that pretrained neural networks, like humans, have internal representations that overrepresent frequent variable values at the expense of certainty for less common values. Furthermore, we demonstrated that optimized readouts of local visual orientation from these networks’ internal representations show similar orientation biases and geometric illusions, just as human subjects.
+                                </div>
+                                  </div>
+                                  
+                                  <br>
+                                  **COURSE PROJECT (CSE 167x/168x, COMPUTER GRAPHICS)**  
+                                  *[\[Simple Python Ray Tracer\]](https://github.com/lingqiz/CSE-167x-RayTracer)  [\[Path Tracer with OptiX\]](https://github.com/lingqiz/CSE-168x-OptiX)* <br>
+                                  
+                                  <div class="row">
+                                    <div class="column">
+                                      <img class="proj-image" src="/assets/img/cse167.png" style="height: 100%; width: 100%; object-fit: contain">
+                                        </div>
+                                        
+                                        <div class="column" markdown="1">
+                                          Basics of physically based rendering: Implementation of basic Whitted Ray-Tracing with Python, and a Monte Carlo Path Tracer with NVIDIA OptiX.
+                                        </div>
+                                          </div>
+                                          
+                                          <br>
+                                          **COURSE PROJECT (STAT 927, BAYESIAN STATISTICS)**  
+                                          *[\[Data+Code\]](https://github.com/zlqzcc/DoublePassBayesian)* <br>
+                                          
+                                          <div class="row">
+                                            <div class="column">
+                                              <img class="proj-image" src="/assets/img/bayesian.png" style="height: 100%; width: 100%; object-fit: contain">
+                                                </div>
+                                                
+                                                <div class="column" markdown="1">
+                                                  Implementation of a hierarchical Bayesian model for parameter estimation of a non-trivial psychophysical experiment (inferring the partitioning of internal vs. external noise in depth perception). Posterior computation with Gibbs sampler nested with Metropolis-Hastings algorithm.
+                                                </div>
+                                                  </div>
+                                                  
